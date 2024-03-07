f7c7d4cb1277c1e4c9ad8169e55fe140a2d9e35c
Natalia-Kalashnikova <dafnisua@gmail.com>
2024-03-07 05:14:33 +0200

    add

--- js/task-2.js
+++ js/task-2.js
@@ -1,55 +1,74 @@
-/**
- * Створи клас Storage, який створюватиме об'єкти для управління складом товарів. 
- * Клас очікує лише один аргумент — початковий масив товарів, який записується 
- * до створеного об'єкта в приватну властивість items.
- * 
- * Оголоси наступні методи класу:
- * -getItems() — повертає масив поточних товарів у приватній властивості items.
- * -addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів 
- * у приватну властивість items об'єкта.
- * -removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє 
- * його з масиву товарів у приватній властивості items об'єкта.
-*/
+// const images = [
+//   {
+//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     alt: 'Elephant Beside on Baby Elephant',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     alt: 'Blue Geeen and Orange Parrot',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     alt: 'Zebras on Zebra',
+//   },
+// ];
 
-// class Storage{
-//   #items;
+/** Impovments 
+ * (document.createElement() and elem.append())*/
 
-//   constructor(items) {
-//     this.#items = items;
-// }
-//   getItems() {
-//     return this.#items;
-//   }
-//   addItem(newItem) {
-//     this.#items.push(newItem);
-//   }
-//   removeItem(itemToRemove) {
-//     this.#items = this.#items.filter(item => item !==itemToRemove);
-//   }
-// }
+//  const galleryList = document.querySelector('.gallery');
 
-/** Improvments (щоб обробити випадки, коли аргумент не передається)*/
+// images.forEach((image) => {
+//   const galleryItem = document.createElement('li');
+//   const galleryImg = document.createElement('img');
 
-class Storage{
-  #items;
+//   galleryImg.src = image.url;
+//   galleryImg.alt = image.alt;
 
-  constructor(items = []) {
-    this.#items = items;
-}
-  getItems() {
-    return this.#items;
-  }
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-  removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !==itemToRemove);
-  }
-}
+//   galleryImg.setAttribute('width', 360);
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+//   galleryItem.append(galleryImg);
+//   galleryList.append(galleryItem);
+// });
+
+/** Impovments */
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },  
+];
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryList = images.map(({url, alt}) => {
+  return `<li><img src='${url}' alt='${alt}' width='360'></li>`;
+});
+
+galleryEl.insertAdjacentHTML('beforeend', galleryList.join(''));
+
+
